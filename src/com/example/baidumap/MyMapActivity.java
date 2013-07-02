f6006d4835c21966c5c0902b139dee74eac5673e
alvin-xu <479359285@qq.com>
2013-07-02 17:04:11 +0800

    baidu map sample
    can show where is myposition

--- src/com/example/baidumap/MyMapActivity.java
+++ src/com/example/baidumap/MyMapActivity.java
@@ -1,18 +1,31 @@
 package com.example.baidumap;
 
+import com.baidu.location.BDLocation;
+import com.baidu.location.BDLocationListener;
+import com.baidu.location.LocationClient;
+import com.baidu.location.LocationClientOption;
 import com.baidu.mapapi.BMapManager;
+import com.baidu.mapapi.map.LocationData;
 import com.baidu.mapapi.map.MapController;
 import com.baidu.mapapi.map.MapView;
+import com.baidu.mapapi.map.MyLocationOverlay;
 import com.baidu.platform.comapi.basestruct.GeoPoint;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.util.Log;
 import android.view.Menu;
 
 public class MyMapActivity extends Activity {
 
 	private BMapManager mBMapMan = null;  
-	private MapView mMapView = null;  
+	private MapView mMapView = null;
+	private LocationClient mLocClient;
+	private MyLocationOverlay myLocationOverlay;
+	private LocationData locData;
+	private boolean isFirstLoc=true;
+	
+	private MyLocationListener myListener=new MyLocationListener();
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -24,13 +37,45 @@ public class MyMapActivity extends Activity {
 		setContentView(R.layout.activity_my_map);  
 		mMapView=(MapView)findViewById(R.id.bmapView);  
 		mMapView.setBuiltInZoomControls(true);  
+		mMapView.setTraffic(true);
+		//mMapView.setSatellite(true);
 		//设置启用内置的缩放控件  
 		MapController mMapController=mMapView.getController();  
 		// 得到mMapView的控制权,可以用它控制和驱动平移和缩放  
-		GeoPoint point =new GeoPoint((int)(39.915* 1E6),(int)(116.404* 1E6));  
+		GeoPoint point =new GeoPoint((int)(28.496* 1E6),(int)(118.182* 1E6));  
 		//用给定的经纬度构造一个GeoPoint，单位是微度 (度 * 1E6)  
 		mMapController.setCenter(point);//设置地图中心点  
 		mMapController.setZoom(12);//设置地图zoom级别  
+		
+		Log.d("LoC", "start begining");
+		System.out.println("end onMedia");
+		
+		 //定位初始化
+        mLocClient = new LocationClient( this );
+        locData = new LocationData();
+        mLocClient.registerLocationListener( myListener );
+        LocationClientOption option = new LocationClientOption();
+        option.setOpenGps(true);//打开gps
+        option.setCoorType("bd09ll");     //设置坐标类型
+        option.setScanSpan(5000);
+        mLocClient.setLocOption(option);
+        Log.d("LoC", "start before");
+        mLocClient.start();
+        Log.d("LoC", "start after");
+        
+        
+        //定位图层初始化
+		myLocationOverlay = new MyLocationOverlay(mMapView);
+		//设置定位数据
+	    myLocationOverlay.setData(locData);
+	    //添加定位图层
+		mMapView.getOverlays().add(myLocationOverlay);
+	
+		myLocationOverlay.enableCompass();
+		//修改定位数据后刷新图层生效
+		mMapView.refresh();
+		Log.d("LoC", "ending");
+		
 	}
 
 	@Override
@@ -65,5 +110,42 @@ public class MyMapActivity extends Activity {
 	        }  
 	       super.onResume();  
 	}  
+	
+	class MyLocationListener implements BDLocationListener{
 
+		@Override
+		public void onReceiveLocation(BDLocation location) {
+			// TODO Auto-generated method stub
+			Log.d("Listener", "begin listener");
+			if (location == null)
+                return ;
+            locData.latitude = location.getLatitude();
+            locData.longitude = location.getLongitude();
+            //如果不显示定位精度圈，将accuracy赋值为0即可
+            locData.accuracy = location.getRadius();
+            locData.direction = location.getDerect();
+            //更新定位数据
+            myLocationOverlay.setData(locData);
+            //更新图层数据执行刷新后生效
+            
+            mMapView.refresh();
+            Log.d("Listener", "after refresh"+locData.latitude+"-"+locData.longitude);
+            //是手动触发请求或首次定位时，移动到定位点
+            if (isFirstLoc){
+            	//移动地图到定位点
+                mMapView.getController().animateTo(new GeoPoint((int)(locData.latitude* 1e6), (int)(locData.longitude *  1e6)));
+                Log.d("Listener", "animate to ....");
+               
+            }
+            //首次定位完成
+            isFirstLoc = false;
+		}
+
+		@Override
+		public void onReceivePoi(BDLocation arg0) {
+			// TODO Auto-generated method stub
+			
+		}
+		
+	}
 }
